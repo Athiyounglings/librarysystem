3776f384649057046e353b6add43253cf3ecb0ad
Athiyounglings <77689934+Athiyounglings@users.noreply.github.com>
2021-01-19 20:36:27 +0200

    first

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//listen for a form
+
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+function submitForm(e){
+    e.preventDefault();
+
+    console.log(aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa);
+}
